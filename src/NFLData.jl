33a691126bc14f6ce7ad237745f352d899a4bb53
John Edwards <edwards1860@gmail.com>
2024-09-07 00:17:24 -0700

    utilities + fixes
    * adding `most_recent_season()` fun
    * cleaning up redundant multiple dispatches
    * cleaning up `load_pbp()`
    * fixing error handling

--- src/NFLData.jl
+++ src/NFLData.jl
@@ -5,11 +5,14 @@ using Preferences
 using DataFrames
 using UrlDownload: urldownload
 using Scratch: @get_scratch!
+using Dates
 
 export cache_data_pref
 export load_players
 export load_pbp
+export most_recent_season
 
+## PREFERENCES
 # set caching preferences, default to true
 function cache_data_pref(pref::Bool)
     @set_preferences!("cache" => pref)
@@ -18,6 +21,35 @@ end
 const cache_data = @load_preference("cache", true)
 
 download_cache = ""
+
+function __init__()
+    global download_cache = @get_scratch!("downloaded_files")
+end
+
+## UTILITIES
+# helper function for computing start of nfl season
+function compute_labor_day(season::Int)
+    earliest = Dates.firstdayofweek(Date(season,9,1))
+    latest = Dates.firstdayofweek(Date(season,9,8))
+    if(month(earliest) == 8)
+        labor_day = latest
+    else
+        labor_day = earliest
+    end
+    return labor_day
+end
+
+# determine start of nfl season
+function most_recent_season(roster::Bool = false)
+    labor_day = compute_labor_day(year(today()))
+    season_opener = labor_day + Day(3)
+    if (!roster && today() >= season_opener) || (roster && month(today()) == 3 && day(today()) >= 15) || (roster && month(today) >= 3)
+        most_rec = year(today())
+    else
+        most_rec = year(today()) - 1
+    end
+    return most_rec
+end
 
 # Downloads a resource, stores it within a scratchspace
 function from_url(url::String)
@@ -33,7 +65,8 @@ function from_url(url::String)
     return df
 end
 
-# Downloads a resource, stores it within a scratchspace
+# for getting data from a specific season
+# can be broadcasted e.g. from_url.(url,2022:2024)
 function from_url(url::String, seasons::Int)
     if cache_data
         fname = joinpath(download_cache, basename(url) * string(seasons) * ".csv.gz")
@@ -47,37 +80,25 @@ function from_url(url::String, seasons::Int)
     return df
 end
 
-function from_url(url::String, seasons::AbstractVector{Int})
-    if cache_data
-        for season in seasons
-            fname = joinpath(download_cache, basename(url) * string(season) * ".csv.gz")
-            if !isfile(fname)
-                download(url * string(season) * ".csv.gz", fname)
-            end
-        end
-        fnames = [joinpath(download_cache, basename(url) * string(season) * ".csv.gz") for season in seasons]
-        df = reduce(vcat, [DataFrame(CSV.File(fname)) for fname in fnames])
-    else
-        df = reduce(vcat, [urldownload(url * string(season) * ".csv.gz") for season in seasons])
-    end 
-    return df
-end
-
-function __init__()
-    global download_cache = @get_scratch!("downloaded_files")
-end
-
+# load all NFL players in NFL DB
 function load_players()
     return from_url("https://github.com/nflverse/nflverse-data/releases/download/players/players")
 end
 
-function load_pbp(seasons)
+# load NFLFastR PBP
+function load_pbp(seasons = most_recent_season())
     if minimum(seasons) < 1999
-        throw(ErrorException(min(seasons),"No PBP data available prior to 1999!"))
-    elseif maximum(seasons) > 2024 # TODO replace with current season logi
-        throw(ErrorException(max(seasons),"No PBP data available after 2024!"))
+        throw(DomainError(minimum(seasons),"No PBP data available prior to 1999!"))
+    elseif minimum(seasons) > 2024 # TODO replace with current season logi
+        throw(DomainError(minimum(seasons),"No PBP data available after 2024!"))
     end
-    return from_url("https://github.com/nflverse/nflverse-data/releases/download/pbp/play_by_play_",seasons)
+    if length(seasons) > 1
+        df = reduce(vcat, from_url.("https://github.com/nflverse/nflverse-data/releases/download/pbp/play_by_play_",seasons))
+    else
+        df = from_url("https://github.com/nflverse/nflverse-data/releases/download/pbp/play_by_play_",seasons)
+    end
+
+    return df
 end
 
 
