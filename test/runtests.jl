fd7143da88014da5938566a5057826ecb3c029b6
John Edwards <edwards1860@gmail.com>
2024-09-07 14:55:57 -0700

    barebones tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,14 @@ using NFLData
 using Test
 
 @testset "NFLData.jl" begin
-    # Write your tests here.
+    # utility tests
+    # labor day check
+    @test size(load_players())[1] > 0
+    @test size(load_pbp())[1] > 0
+    @test size(load_contracts())[1] > 0
+    @test size(load_draft_picks())[1] > 0
+    @test size(load_espn_qbr())[1] > 0
+    @test size(load_ff_playerids())[1] > 0
+    @test size(load_ff_rankings())[1] > 0
+    @test size(most_recent_season())[1] > 0
 end
