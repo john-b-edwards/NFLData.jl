8cdee0d1457f9828f03b9f5e3bbb488d117c8941
John Edwards <edwards1860@gmail.com>
2024-09-14 13:45:15 -0700

    add ffverse

--- src/NFLData.jl
+++ src/NFLData.jl
@@ -9,6 +9,9 @@ using .helpers
 include("getdata.jl")
 using .getdata
 
+include("ffverse.jl")
+using .ffverse
+
 export cache_data_pref
 export load_players
 export load_pbp
@@ -70,39 +73,6 @@ function load_espn_qbr(summary_type = "season")
         throw(DomainError(summary_type,"Please pass in one of \"season\" or \"week\" for the argument `summary_type`!"))
     end 
     df = from_url("https://github.com/nflverse/nflverse-data/releases/download/espn_data/qbr_$summary_type" * "_level")
-    return df
-end
-
-# load fantasy player ids
-function load_ff_playerids()
-    return from_url("https://github.com/dynastyprocess/data/raw/master/files/db_playerids", file_type = ".csv")
-end
-
-# load latest fantasy player rankings
-function load_ff_rankings(type = "draft")
-    if !(type in ["draft","week","all"])
-        throw(DomainError(type,"Please pass in one of \"draft\", \"week\", or \"all\" for the argument `type`!"))
-    end
-    if type == "draft"
-        df = from_url("https://github.com/dynastyprocess/data/raw/master/files/db_fpecr_latest",file_type = ".csv")
-    elseif type == "week"
-        df = from_url("https://github.com/dynastyprocess/data/raw/master/files/fp_latest_weekly",file_type = ".csv")
-    elseif type == "all"
-        df = from_url("https://github.com/dynastyprocess/data/raw/master/files/db_fpecr")
-    end
-    return df
-end
-
-# load ff opportunity stats
-function load_ff_opportunity(seasons = most_recent_season(), stat_type = "weekly", model_version = "latest")
-    seasons = check_years(seasons, 2006, "FF opportunity data")
-    if !(stat_type in ["weekly","pbp_pass","pbp_rush"])
-        throw(DomainError(stat_type,"Please pass in one of \"weekly\",\"pbp_pass\",\"pbp_rush\" for the argument `stat_type`!"))
-    end
-    if !(model_version in ["latest","v1.0.0"])
-        throw(DomainError(stat_type,"Please pass in one of \"latest\" or \"v1.0.0\" for the argument `model_version`!"))
-    end
-    df = reduce(vcat, from_url.("https://github.com/ffverse/ffopportunity/releases/download/$model_version-data/ep_$stat_type" * "_",seasons))
     return df
 end
 
